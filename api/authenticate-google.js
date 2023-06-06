0013b94d9826c48d0d56c22733bb1fad1167ce0a
meng <mengzhou44@gmail.com>
2023-06-06 16:23:51 -0600

    clean up

--- api/authenticate-google.js
+++ api/authenticate-google.js
@@ -13,12 +13,10 @@ const identityPoolId = process.env.COGNITO_IDENTITY_POOL_ID;
 
 exports.handler = async (event) => {
     try {
-        console.log({headers: event.headers})
+       
         let id_token = util.getIdToken(event.headers);
-        console.log({id_token})
-        console.log('step1')
+       
         let params = {
-            AccountId: '681206886478',
             IdentityPoolId: identityPoolId,
             Logins: {
                 'accounts.google.com': id_token
@@ -26,7 +24,7 @@ exports.handler = async (event) => {
         };
 
         let data = await cognitoidentity.getId(params).promise();
-        console.log('step2')
+ 
 
         params = {
             IdentityId: data.IdentityId,
@@ -36,18 +34,17 @@ exports.handler = async (event) => {
         };
 
         data = await cognitoidentity.getCredentialsForIdentity(params).promise();
-        console.log('step3')
+       
         let decoded = jwtDecode(id_token);
         data.user_name = decoded.name;
-        console.log('step4)')
-       
+         
         return {
             statusCode: 200,
             headers: util.getResponseHeaders(),
             body: JSON.stringify(data)
         };
     } catch (err) {
-        console.log("Error", err);
+       
         return {
             statusCode: err.statusCode ? err.statusCode : 500,
             headers: util.getResponseHeaders(),
